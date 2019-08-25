2e2946f5deb8253a99ad5559472e75b86177dfa4
Wang Xian <wangxian_drew@163.com>
2019-08-25 20:04:20 +0800

    修改消息发布

--- src/components/account/MessageCenter.vue
+++ src/components/account/MessageCenter.vue
@@ -2,16 +2,16 @@
   <div>
     <el-tabs v-model="activeTab">
       <el-tab-pane label="收到的消息" name="message">
-        <el-row :gutter="10">
+        <el-row>
           <el-button-group>
             <el-button type="success" size="small">标记为已读</el-button>
             <el-button type="danger" size="small">标记为未读</el-button>
           </el-button-group>
           <el-button type="warning" size="small">全部标记为已读</el-button>
         </el-row>
-        <el-row :gutter="10">
+        <el-row>
         </el-row>
-        <el-row :gutter="10">
+        <el-row>
           <el-table
             :data="receiveMessageTableData"
             tooltip-effect="dark"
@@ -35,7 +35,7 @@
             <el-table-column
               prop="messageContent.message"
               label="消息内容"
-              show-overflow-tooltip
+              :showOverflowTooltip=true
               width="180">
             </el-table-column>
             <el-table-column
@@ -71,32 +71,133 @@
             </el-table-column>
           </el-table>
         </el-row>
-        <el-row :gutter="10">
-          <el-row style="margin-top: 20px">
-            <div style="text-align: center">
-              <el-pagination
-                background
-                @size-change="receiveMessageHandleSizeChange"
-                @current-change="receiveMessageHandleCurrentChange"
-                :current-page="receiveCurrentPage"
-                :page-sizes="[15, 50, 100, 500]"
-                :page-size="receivePageSize"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="receiveTotalElements">
-              </el-pagination>
-            </div>
-          </el-row>
+        <el-row style="margin-top: 20px">
+          <div style="text-align: center">
+            <el-pagination
+              background
+              @size-change="receiveMessageHandleSizeChange"
+              @current-change="receiveMessageHandleCurrentChange"
+              :current-page="receiveCurrentPage"
+              :page-sizes="[15, 50, 100, 500]"
+              :page-size="receivePageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="receiveTotalElements">
+            </el-pagination>
+          </div>
         </el-row>
       </el-tab-pane>
       <el-tab-pane label="消息管理" name="message-manager">
-        <el-row :gutter="10">
+        <el-row>
           <el-button-group>
             <el-button type="danger" icon="el-icon-delete" size="small">批量删除</el-button>
             <el-button type="primary" size="small" icon="el-icon-plus" @click="showPublishMessageContentForm">发布消息
             </el-button>
           </el-button-group>
         </el-row>
-        <el-row :gutter="10">
+        <el-row>
+          <el-form :inline="true" :model="searchPublishMessageForm" ref="searchPublishMessageForm" size="small">
+            <el-form-item>
+              <el-input v-model="searchPublishMessageForm.condition" placeholder="标题/内容模糊查询"></el-input>
+            </el-form-item>
+            <el-form-item label="开始时间">
+              <el-date-picker
+                v-model="searchPublishMessageForm.startTime"
+                type="datetime"
+                placeholder="选择发送时间"
+                value-format="yyyy-MM-dd HH:mm:ss"
+                align="right"
+                :picker-options="pickerOptions">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="结束时间">
+              <el-date-picker
+                v-model="searchPublishMessageForm.endTime"
+                type="datetime"
+                placeholder="选择发送时间"
+                value-format="yyyy-MM-dd HH:mm:ss"
+                align="right"
+                :picker-options="pickerOptions">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" icon="el-icon-search" @click="searchPublishMessage">搜索</el-button>
+            </el-form-item>
+          </el-form>
+        </el-row>
+        <el-row>
+          <el-table
+            :data="publishMessageTableData"
+            tooltip-effect="dark"
+            size="small"
+            border
+            style="width: 100%">
+            <el-table-column
+              type="selection"
+              width="55">
+            </el-table-column>
+            <el-table-column
+              prop="id"
+              label="id"
+              width="80">
+            </el-table-column>
+            <el-table-column
+              prop="title"
+              label="标题"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="message"
+              label="消息内容"
+              :showOverflowTooltip=true
+              width="200">
+            </el-table-column>
+            <el-table-column
+              prop="level"
+              label="消息级别"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="level"
+              label="消息类型"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="createAt"
+              label="接收时间"
+              width="140">
+            </el-table-column>
+            <el-table-column
+              prop="modifiedAt"
+              label="状态改变时间"
+              width="140">
+            </el-table-column>
+            <el-table-column
+              fixed="right"
+              label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" @click="queryInfo(scope.row)">详情</el-button>
+                <el-button size="mini" @click="queryInfo(scope.row)" type="primary">编辑
+                </el-button>
+                <el-button size="mini" @click="queryInfo(scope.row)" type="danger">删除
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+
+        </el-row>
+        <el-row style="margin-top: 20px">
+          <div style="text-align: center">
+            <el-pagination
+              background
+              @size-change="receiveMessageHandleSizeChange"
+              @current-change="receiveMessageHandleCurrentChange"
+              :current-page="publishCurrentPage"
+              :page-sizes="[15, 50, 100, 500]"
+              :page-size="publishPageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="publishTotalElements">
+            </el-pagination>
+          </div>
         </el-row>
         <el-dialog
           :title="messageContentFormTitle"
@@ -188,6 +289,37 @@
                     title: '',
                     message: '',
                     level: ''
+                },
+                publishMessageTableData: [],
+                publishCurrentPage: 1,
+                publishTotalElements: 0,
+                publishPageSize: 15,
+                searchPublishMessageForm: {
+                    condition: '',
+                    startTime: '',
+                    endTime: ''
+
+                }, pickerOptions: {
+                    shortcuts: [{
+                        text: '今天',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    }, {
+                        text: '昨天',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit('pick', date);
+                        }
+                    }, {
+                        text: '一周前',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', date);
+                        }
+                    }]
                 }
             }
         },
@@ -200,6 +332,33 @@
             },
             showPublishMessageContentForm() {
                 this.messageContentFormVisible = true;
+            },
+            searchPublishMessage() {
+                const _this = this;
+                let requestUrl = "/api/v1/messages/publish/search?page=" + _this.receiveCurrentPage + "&size=" + _this.receivePageSize;
+                if (this.searchPublishMessageForm.condition !== '') {
+                    requestUrl = requestUrl + '&condition=' +
+                        this.searchPublishMessageForm.title;
+                }
+                if (this.searchPublishMessageForm.startTime !== '') {
+                    requestUrl = requestUrl + '&startTime=' +
+                        this.searchPublishMessageForm.startTime;
+                }
+                if (this.searchPublishMessageForm.endTime !== '') {
+                    requestUrl = requestUrl + '&endTime=' +
+                        this.searchPublishMessageForm.endTime;
+                }
+                _this.getRequest(requestUrl)
+                    .then(function (response) {
+                        const data = response.data.data;
+                        _this.publishMessageTableData = data.content;
+                        _this.publishTotalElements = data.totalElements;
+                        _this.publishPageSize = data.size;
+                        _this.publishCurrentPage = data.number + 1;
+
+                    }).catch(error => {
+                    console.log("search file error:" + error);
+                });
             },
             publishMessage() {
                 this.$refs.messageContentForm.validate(valid => {
@@ -218,7 +377,9 @@
                                 type: 'success',
                                 message: '发送成功',
                                 showClose: true
-                            })
+                            });
+                            _this.searchPublishMessage();
+                            _this.messageContentFormVisible = false;
                         }).catch(error => {
                             console.log("error:" + error.response);
                         })
@@ -274,6 +435,7 @@
         },
         mounted() {
             this.searchReceiveMessage();
+            this.searchPublishMessage();
         }
     }
 </script>
