a5e70d9f55ea2bb0673cb0741e1573d70bc35ca9
Wang Xian <wangxian_drew@163.com>
2019-09-07 09:51:37 +0800

    '修改菜单'

--- src/util/commonApi.js
+++ src/util/commonApi.js
@@ -1,18 +1,14 @@
 import {getRequest} from "./api";
 
-export const getAllEnterprise = () => {
-  let requestUrl = "/api/v1/enterprises";
-  console.log('common get enterprise');
-  getRequest(requestUrl).then(function (response) {
-    return response.data.data;
-  })
-};
 export const initMenu = (store) => {
   if (store.state.menus.length > 0) {
     return;
   }
   getRequest("/api/v1/menus/generate").then(resp => {
-    const data = resp.data.data;
+    let data = resp.data.data;
+    if (data === null) {
+      data = [];
+    }
     store.commit('initMenu', data);
   })
 };
